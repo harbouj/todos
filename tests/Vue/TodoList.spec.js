87071c57d2e315ea84d519f6f8aab617e3848b38
simo <m.harbouj@gmail.com>
2018-12-10 16:40:43 +0100

    Test js: it shows the todos from the api

--- tests/Vue/TodoList.spec.js
+++ tests/Vue/TodoList.spec.js
@@ -18,3 +18,25 @@ it("renders the correct title on the page", () => {
 
     expect(wrapper.html()).toContain("Todo List");
 });
+
+it("shows the todos fetched from the api", done => {
+    let wrapper = mount(TodoList);
+
+    moxios.stubRequest("/todos", {
+        status: 200,
+        response: [
+            {
+                id: 10,
+                text: "task1",
+                finished: false,
+                created_at: "2018-01-10 00:00:00",
+                updated_at: "2018-01-10 00:00:00"
+            }
+        ]
+    });
+
+    moxios.wait(() => {
+        expect(wrapper.html()).toContain("task1");
+        done();
+    });
+});
