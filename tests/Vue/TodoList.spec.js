1b53ce192b919b431b28b3a0883f61b323a3933a
simo <m.harbouj@gmail.com>
2018-12-11 09:56:54 +0100

    Test js: empty state if no todos are fetched

--- tests/Vue/TodoList.spec.js
+++ tests/Vue/TodoList.spec.js
@@ -1,6 +1,6 @@
 import { shallow, mount } from "vue-test-utils";
 import expect from "expect";
-import TodoList from "../../resources/js/components/todo.vue";
+import TodoList from "../../resources/js/components/todo-list.vue";
 import moxios from "moxios";
 
 describe("TodoList", () => {
@@ -11,32 +11,46 @@ describe("TodoList", () => {
     afterEach(() => {
         moxios.uninstall(axios);
     });
-});
 
-it("renders the correct title on the page", () => {
-    let wrapper = shallow(TodoList);
+    it("renders the correct title on the page", () => {
+        let wrapper = shallow(TodoList);
 
-    expect(wrapper.html()).toContain("Todo List");
-});
-
-it("shows the todos fetched from the api", done => {
-    let wrapper = mount(TodoList);
-
-    moxios.stubRequest("/todos", {
-        status: 200,
-        response: [
-            {
-                id: 10,
-                text: "task1",
-                finished: false,
-                created_at: "2018-01-10 00:00:00",
-                updated_at: "2018-01-10 00:00:00"
-            }
-        ]
+        expect(wrapper.html()).toContain("Todo List");
     });
 
-    moxios.wait(() => {
-        expect(wrapper.html()).toContain("task1");
-        done();
+    it("shows the todos fetched from the api", done => {
+        let wrapper = mount(TodoList);
+
+        moxios.stubRequest("/todos", {
+            status: 200,
+            response: [
+                {
+                    id: 10,
+                    text: "task1",
+                    finished: false,
+                    created_at: "2018-01-10 00:00:00",
+                    updated_at: "2018-01-10 00:00:00"
+                }
+            ]
+        });
+
+        moxios.wait(() => {
+            expect(wrapper.html()).toContain("task1");
+            done();
+        });
+    });
+
+    it("shows the empty state if no todos are fetched from the api", done => {
+        let wrapper = mount(TodoList);
+
+        moxios.stubRequest("/todos", {
+            status: 200,
+            response: []
+        });
+
+        moxios.wait(() => {
+            expect(wrapper.html()).toContain("There are no todos");
+            done();
+        });
     });
 });
